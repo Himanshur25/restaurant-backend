66abb57fc5c24ab05c7117cbdd3acef732b0e345
Himanshur25 <himanshurathi@gkmit.co>
2022-11-10 21:52:56 +0530

    final commmit

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const dotenv=require('dotenv')
 dotenv.config()
 const mysql=require('mysql')
 const cors=require('cors')
-//const jwt=require('jsonwebtoken ')
-//const cookiePraser=require('cookie-parser')
+const path =require('path')
+const jwt=require('jsonwebtoken')
+
+
 app.use(express.json())
 app.use(cors())
 const con = mysql.createConnection({
@@ -13,27 +15,18 @@ const con = mysql.createConnection({
     database:process.env.Database,
     user:process.env.User,
     password:process.env.Password,
-    port:process.env.Port
+    port:process.env.Port,
+    multipleStatements:true
 
   });
-  console.log(' :>> ', {
-    host:process.env.Host,
-    database:process.env.Database,
-    user:process.env.User,
-    password:process.env.Password,
-    port:process.env.Port
 
-  });
   con.connect((err)=> {
     if (err) throw err;
     console.log("Connected!");
   });
-  const port=process.env.PORT || 5000
-  app.listen(port,()=>{
-    console.log(`Server is running on ${port}`)
-})
+ 
 
-//app.use(cookiePraser())
+
 app.get('/',(req,res)=>{
     res.send('Welcome to my server')
 })
@@ -54,20 +47,25 @@ app.get('/api/user',(req,res)=>{
     })
 
 })
-
+// app.get('/api/user/:id',(req,res)=>{
+//     const userr=user.find(c=>c.id===parseInt(req.params.id));
+//     if(!userr)res.status(404).send("Error")
+//     res.send(userr);
+// })
 app.post("/api/register",(req,res)=>{
     
-    console.log(req);
+    
     const fullname=req.body.fullname
     const email=req.body.email
     const password=req.body.password
-    console.log(fullname+" "+email+" "+password);
+    //console.log(fullname+" "+email+" "+password)
+    const user={name:fullname}
+    const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN)
+    res.json({accessToken:accessToken})
     con.query("insert into user(fullname,email,password) values(?,?,?)",[fullname,email,password],(err,result)=>{
         if(err){
-
             return res.status(500).json({
-                error:"Internal server error",
-                message:"hello"
+                error:"Internal server error"
             })
         }
         if(!fullname || !email || !password){
@@ -80,6 +78,18 @@ app.post("/api/register",(req,res)=>{
         })
     })
 })
+function authenticateuser(req,res,next)
+{
+    const authHeader=req.headers("authorization")
+    const token=authHeader && authHeader.split('')[1]
+    if(token==NULL) return res.sendstatus(401)
+    jwt.verify(token,process.env.ACCESS_TOKEN,(err,user)=>{
+        if(err) return res.sendstatus(403)
+        req.user=user
+        next()
+    })
+}
+
 
 app.post("/api/login",(req,res)=>{
     const email=req.body.email;
@@ -108,23 +118,95 @@ app.post("/api/login",(req,res)=>{
         })
     
     })
-    // app.post('/api/Food',(req,res)=>{
-    //     const food_id=req.body.food_id
-    //     const food_name=req.body.food_name
-    //     const food_price=req.body.food_price
-    //     con.query("insert into Food(food_id,food_name,food_price) values(?,?,?)",[food_id,food_name,food_price],(err,result)=>{
-    //         if(err){
-    //             return res.status(500).json({
-    //                 error:"Internal server error"
-    //             })
-    //         }
-    //         if(!food_id || !food_name || !food_price){
-    //             return res.status(400).json({
-    //                 error:"fill the credential properly"
-    //             })
-    //         }
-    //         return res.status(201).json({
-    //             message:"Item Added successfully"
-    //         })
-    //     })
-    // })
+    
+    app.post('/api/foods',(req,res)=>{
+        const {name,price}=req.body;
+        con.query("insert into foods(food_name,food_price,food_image) values(?,?,?)",[name,price,"./images/Vanillashake.jpeg"],(err,result)=>{
+                        if(err){
+                            return res.json({
+                                error:err
+                            })
+                           
+                        }
+                        return res.status(201).json({
+                            message:"Successfully added"
+                        })
+            })
+        })  
+
+    app.get('/api/foods',(req,res)=>{
+        con.query("select * from foods",(err,result)=>{
+            if(err){
+                return res.status(500).json({
+                        message:"Internal server error"
+                    })
+                }
+                return res.status(200).json(result)
+        })
+    })
+
+
+    app.post('/api/cart',(req,res)=>{
+        const food_id=req.body.food_id
+         const id=req.body.user_id
+        
+
+         if(!food_id || !id ){
+            return res.status(400).json({"error":"error"})
+         }
+
+         
+        con.query("select * from cart where food_id=?",[food_id],(err,result2)=>{
+            if(err){
+                return res.status(500).json({
+                    error:"Internal server error"
+                })
+            }
+            if(result2.length>0){
+                con.query("update cart set quantity=? where cart_id=?",[result2[0].quantity+1,result2[0].cart_id],(err,result1)=>{
+                    if(err){
+                        return res.status(500).json({
+                            error:"Internal server error"
+                        })
+                    }
+                    return res.status(200).json({
+                        message:"quantity updated"
+                    })
+                })
+            }else{
+        
+         con.query("insert into cart(quantity,id,food_id) values(?,?,?)",[1,id,food_id],(err,result)=>{
+             if(err){
+                 return res.status(500).json({
+                     error:"Internal server error"
+                 })
+             }
+
+             
+             return res.status(200).json({
+                 message:"Item Added to cart successfully"
+             })
+         })
+        }
+        })
+    })
+        
+    
+     app.get('/api/cart',(req,res)=>{
+        con.query("select food_name,food_price,quantity from cart,foods where foods.food_id=cart.food_id",(err,result)=>{
+            if(err){
+                return res.status(500).json({
+                        message:"Internal server error"
+                    })
+                }
+                return res.status(200).json(result)
+        })
+    
+    })
+
+
+
+    const port=process.env.PORT || 5000
+    app.listen(port,()=>{
+      console.log(`Server is running on ${port}`)
+  })
